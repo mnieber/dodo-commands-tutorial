1703c1be02ff78e4c3a50451e6c33928b847f135
Maarten Nieber <hallomaarten@yahoo.com>
2017-07-21 12:18:48 +0200

    Inline _get_variable_list

--- extra/dodo_commands/tutorial_commands/cmake.py
+++ extra/dodo_commands/tutorial_commands/cmake.py
@@ -2,16 +2,13 @@
 from dodo_commands.system_commands import DodoCommand
 
 class Command(DodoCommand):  # noqa
-    def _get_variable_list(self, prefix):
-        return [
-            prefix + "%s=%s" % key_val for key_val in
-            self.get_config('/CMAKE/variables').items()
-        ]
-
     def handle_imp(self, **kwargs):  # noqa
         self.runcmd(
             ["cmake"] +
-            self._get_variable_list("-D") +
+            [
+                "-D%s=%s" % x for x in
+                self.get_config('/CMAKE/variables').items()
+            ] +
             [self.get_config("/ROOT/src_dir")],
             cwd=self.get_config("/ROOT/build_dir")
         )
