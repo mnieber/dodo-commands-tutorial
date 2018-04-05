d40ec50a179b87d0f413f78637e953a99b80d095
Maarten Nieber <hallomaarten@yahoo.com>
2018-04-05 02:07:57 +0200

    Update cmake.py to latest Dodo Commands version

--- extra/dodo_commands/tutorial_commands/cmake.py
+++ extra/dodo_commands/tutorial_commands/cmake.py
@@ -1,16 +1,23 @@
-"""Configure code with CMake."""
-from dodo_commands.system_commands import DodoCommand
+from argparse import ArgumentParser
+from dodo_commands.framework import Dodo
 
-class Command(DodoCommand):  # noqa
-    docker_options = [('name', 'cmake')]
 
-    def handle_imp(self, **kwargs):  # noqa
-        self.runcmd(
-            ["cmake"] +
-            [
-                "-D%s=%s" % x for x in
-                self.get_config('/CMAKE/variables').items()
-            ] +
-            [self.get_config("/ROOT/src_dir")],
-            cwd=self.get_config("/ROOT/build_dir")
-        )
+def _args():  # noqa
+    parser = ArgumentParser(description='Configure code with CMake')
+    args = Dodo.parse_args(parser)
+    args.src_dir = Dodo.get_config("/ROOT/src_dir")
+    args.build_dir = Dodo.get_config("/ROOT/build_dir")
+    return args
+
+
+if Dodo.is_main(__name__):
+    args = _args()
+    Dodo.runcmd(
+        ["cmake"] +
+        [
+            "-D%s=%s" % x for x in
+            Dodo.get_config('/CMAKE/variables').items()
+        ] +
+        [args.src_dir],
+        cwd=args.build_dir
+    )
